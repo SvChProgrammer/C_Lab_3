94ecbe4bb297703bbf815e6c59c0acc76d1a0871
Svetlana <lana.chebotareva.95@mail.ru>
2022-08-16 13:12:09 +0400

    final commit task_21

--- Program.cs
+++ Program.cs
@@ -14,10 +14,11 @@ int num,r,sum=0, newValue;
 
 /* 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.*/
 
-Console.WriteLine("\n Задача 21");
+
 int x1 = Coordinate("x", "A");
 int y1 = Coordinate("y", "A");
 int z1 = Coordinate("z", "A");
+
 int x2 = Coordinate("x", "B");
 int y2 = Coordinate("y", "B");
 int z2 = Coordinate("z", "B");
@@ -30,14 +31,13 @@ int Coordinate(string coorName, string pointName)
 
 double Distance(double x1, double x2, 
                 double y1, double y2, 
-                double z1, double z2){
-return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
+                double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
 }
 
-double length =  Math.Round (Distance(x1, x2, y1, y2, z1, z2), 2 );
-Console.WriteLine($"Расстояние =  {length}");
+double distance =  Math.Round (Distance(x1, x2, y1, y2, z1, z2), 2);
+Console.WriteLine($"Расстояние =  {distance}");
 
 
 /*Задача 23
