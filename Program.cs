a756729b3f356c9a6d95a387a7948fe7893ca414
Svetlana <lana.chebotareva.95@mail.ru>
2022-08-16 11:19:25 +0400

    task_23

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+*/
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+ //int i = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine("Результат");
+        Console.WriteLine("{0}^3 = {1}", i, Math.Pow(i, 3));
+     }
