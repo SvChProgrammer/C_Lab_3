6a1b90a0d7ae7bbd533e4ea45d9af74347e376e0
Svetlana <lana.chebotareva.95@mail.ru>
2022-08-16 11:09:09 +0400

    task_191

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*Задача 19
 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.*/
-int num,r,sum=0,t;
+int num,r,sum=0, newValue;
     Console.WriteLine("Введите число: ");
          num = Convert.ToInt32(Console.ReadLine());  
-            for(t=num; num!=0; num=num/10){
+            for(newValue=num; num!=0; num=num/10){
                 r=num % 10;
                 sum=sum*10+r;
                 }
-        if(t==sum)
-             Console.Write("{0} Это палиндром!.\n",t);
+        if(newValue==sum)
+             Console.Write("Число {0} Это палиндром!.\n",newValue);
         else
-              Console.Write("{0} Это не палиндром!.\n",t);
+              Console.Write("Число {0} Это не палиндром!.\n",newValue);
 
