548139cf27e53ca7205157c8547fe6c2d8f79f1c
Svetlana <lana.chebotareva.95@mail.ru>
2022-08-16 11:14:28 +0400

    Update task_21

--- Program.cs
+++ Program.cs
@@ -14,14 +14,14 @@ int Coordinate(string coorName, string pointName)
     return Convert.ToInt16(Console.ReadLine());
 }
 
-double Decision(double x1, double x2, 
+double Distance(double x1, double x2, 
                 double y1, double y2, 
                 double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+return Math.Sqrt(Math.Pow((x2-x1), 2) + 
                    Math.Pow((y2-y1), 2) + 
                    Math.Pow((z2-z1), 2));
 }
 
-double length =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
-Console.WriteLine($"Расстояние равно  {length}");
+double length =  Math.Round (Distance(x1, x2, y1, y2, z1, z2), 2 );
+Console.WriteLine($"Расстояние =  {length}");
 
